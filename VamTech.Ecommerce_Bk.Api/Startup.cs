73ff72613efd11c74186daddd2944fd22b292170
Fernando Sanchez <fer24sanchez@hotmail.com>
2020-09-25 21:18:20 -0300

    cadena de conexion y ubicacion de swagger

--- VamTech.Ecommerce_Bk.Api/Startup.cs
+++ VamTech.Ecommerce_Bk.Api/Startup.cs
@@ -134,7 +134,7 @@ namespace VamTech.Ecommerce.Api
 
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Vamtech Ecommerce API V1");
+                options.SwaggerEndpoint("../swagger/v1/swagger.json", "Vamtech Ecommerce API V1");
                 options.RoutePrefix = string.Empty;
             });
 
