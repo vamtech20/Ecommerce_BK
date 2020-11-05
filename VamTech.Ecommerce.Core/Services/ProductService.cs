0b3ae8e1e20a176d42cf13a540ed3ac3016d8cb4
Fernando Sanchez <fer24sanchez@hotmail.com>
2020-11-05 02:34:54 -0300

    fix nombre de parametro

--- VamTech.Ecommerce.Core/Services/ProductService.cs
+++ VamTech.Ecommerce.Core/Services/ProductService.cs
@@ -40,12 +40,12 @@ namespace VamTech.Ecommerce.Core.Services
             filters.TextToFind = filters.TextToFind is null ? string.Empty : filters.TextToFind;
             filters.IsFeatured = filters.IsFeatured is null ? -1 : filters.IsFeatured;
             filters.CategoryId = filters.CategoryId is null ? 0 : filters.CategoryId;
-            filters.SubcatategoryId = filters.SubcatategoryId is null ? 0 : filters.SubcatategoryId;
+            filters.SubcategoryId = filters.SubcategoryId is null ? 0 : filters.SubcategoryId;
 
             var Products = _unitOfWork.ProductRepository.GetAll().Where(x => (x.IsFeatured == filters.IsFeatured || filters.IsFeatured ==-1)
                                       && (x.LongDesc.ToUpper().Contains(filters.TextToFind.ToUpper()) || filters.TextToFind.Trim().Length == 0)
                                       &&(x.Categories.Any(x=> x.Subcategory.CategoryId == filters.CategoryId || filters.CategoryId == 0))
-                                      && (x.Categories.Any(x => x.SubcategoryId == filters.SubcatategoryId || filters.SubcatategoryId == 0))
+                                      && (x.Categories.Any(x => x.SubcategoryId == filters.SubcategoryId || filters.SubcategoryId == 0))
                                      );
 
             var pagedProducts = PagedList<Product>.Create(Products, filters.PageNumber, filters.PageSize);
